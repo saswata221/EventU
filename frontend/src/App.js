20e576f39a6d82d1042817f7d8518c331c702c41
saswata221 <saswatamahato652@gmail.com>
2025-09-04 19:11:27 +0530

    Added login/signup pop up

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ScrollToTop from "./Components/JsCompo/ScrollTop";
 import Movie from "./Pages/Movie";
-import LogIn from "./Pages/LogIn";
+// import LogIn from "./Pages/LogIn";
 import Booking from "./Pages/Booking";
 import ListShow from "./Pages/ListShow";
 import EventInfo from "./Pages/EventInfo";
@@ -24,7 +24,7 @@ function App() {
         <Route path="/Event" element={<Event />}></Route>
         <Route path="/booking/:id" element={<Booking />}></Route>
         <Route path="/bookings/:id" element={<Booking />}></Route>{" "}
-        <Route path="/login" element={<LogIn />}></Route>
+        {/* <Route path="/login" element={<LogIn />}></Route> */}
         <Route path="/listshow" element={<ListShow />}></Route>
         <Route path="/eventinfo/:id" element={<EventInfo />}></Route>
         <Route path="/yourbookings" element={<YourBookings />} />
--- frontend/src/Components/JsCompo/Navbar.jsx
+++ frontend/src/Components/JsCompo/Navbar.jsx
@@ -6,54 +6,65 @@ import { FaRegUserCircle } from "react-icons/fa";
 import { TiThMenu } from "react-icons/ti";
 import Search from "./SearchBar";
 
-function Navbar() {
-  return <div className=" bg-[#]">
-    <div className="h-[83px] w-full flex flex-wrap bg-[#1A1A2E] text-white justify-between">
-      <div className="pl-20 flex items-center m-">
-        <Link to="/">
-      <img 
-        src={logo} 
-        alt="logo" 
-        className="h-fit w-[60px] scale-150 cursor-pointer" 
-      />
-    </Link>
-      </div>
-      {/* <div> */}
-        <Search/>
-      {/* </div> */}
-      <div className="flex text-[1rem] items-center gap-10 pr-10">
-        <p className="flex items-center gap-1">
-          <IoLocationSharp />
-          Location
-          <TiArrowSortedDown />
-        </p>
-        <Link to="/event"><p>Events</p></Link>
-        <p>Contact Us</p>
-        <p className="bg-[#EF233C] px-3 py-1 text-white rounded-3xl">Sign In</p>
-        <FaRegUserCircle className="scale-150" />
-        <TiThMenu className="scale-150" />
-      </div>
-    </div>
-    <div className="bg-[#51557E] text-white flex justify-between">
-      <div className="flex gap-10 px-5">
-        <p className="flex items-center">
-          Generes <TiArrowSortedDown />
-        </p>
-        <p>TV Shows</p>
-        <Link to="/movielist">
-        <p>Movies</p>
-        </Link>
-        <p>SPORTS</p>
-      </div>
-      <div className="flex gap-10 px-5">
-        <Link to="/listshow"><p>List Your Show</p></Link>
-        <p>Corporates</p>
-        <p>Offers</p>
-        <p>Gift Cards</p>
-      </div>
-    </div>
-  </div>
-}
+// Accept onLoginClick from Home to open the modal (no navigation to /login)
+function Navbar({ onLoginClick }) {
+  return (
+    <div className="bg-[#]">
+      <div className="flex h-[83px] w-full flex-wrap items-center justify-between bg-[#1A1A2E] text-white">
+        <div className="pl-20">
+          <Link to="/">
+            <img
+              src={logo}
+              alt="logo"
+              className="h-fit w-[60px] scale-150 cursor-pointer"
+            />
+          </Link>
+        </div>
 
+        <Search />
+
+        <div className="flex items-center gap-10 pr-10 text-[1rem]">
+          <p className="flex items-center gap-1">
+            <IoLocationSharp />
+            Location
+            <TiArrowSortedDown />
+          </p>
+          <Link to="/event"><p>Events</p></Link>
+          <p>Contact Us</p>
+
+          {/* Opens the login modal */}
+          <button
+            onClick={onLoginClick}
+            className="rounded-3xl bg-[#EF233C] px-3 py-1 text-white"
+          >
+            Sign In
+          </button>
+
+          <FaRegUserCircle className="scale-150" />
+          <TiThMenu className="scale-150" />
+        </div>
+      </div>
+
+      <div className="flex justify-between bg-[#51557E] text-white">
+        <div className="flex gap-10 px-5">
+          <p className="flex items-center">
+            Generes <TiArrowSortedDown />
+          </p>
+          <p>TV Shows</p>
+          <Link to="/movielist">
+            <p>Movies</p>
+          </Link>
+          <p>SPORTS</p>
+        </div>
+        <div className="flex gap-10 px-5">
+          <Link to="/listshow"><p>List Your Show</p></Link>
+          <p>Corporates</p>
+          <p>Offers</p>
+          <p>Gift Cards</p>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default Navbar;
